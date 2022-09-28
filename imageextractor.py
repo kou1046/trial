8f9cd125149e1052d9adda7777feeabda10a6347
kou1046 <iwashiro0517@yahoo.co.jp>
2022-09-28 16:00:34 +0900

    animateMatchTmeplate関数を追加

--- imageextractor.py
+++ imageextractor.py
@@ -1,12 +1,18 @@
 from __future__ import annotations
 import os
-from typing import Callable
+import sys
+from typing import Callable, Generator
 import cv2
 import tkinter as tk
 import matplotlib.pyplot as plt
 import matplotlib.axes as mpl_axes
-from matplotlib.image import AxesImage
+from matplotlib.colors import Normalize
+from matplotlib.cm import ScalarMappable, get_cmap
+import matplotlib.figure as mpl_fig
+from matplotlib.animation import FuncAnimation
 import numpy as np
+
+sys.path.append(os.path.join(__file__, '..', '..'))
 
 from mycommon import mywidget
 
@@ -41,26 +47,51 @@ class ImageExtractor(tk.Tk):
             tk.Button(self, text='完了', command=self.destroy).pack()
         self.result = extracted_img
 
-def shift_extracted_img(img:np.ndarray, extracted_img:np.ndarray, ax:mpl_axes.Axes, row_step:int=1, col_step:int=1) -> list[AxesImage]:
+def animateMatchTemplate(img:np.ndarray, extracted_img:np.ndarray, row_step:int=1, col_step:int=1,
+                         cmap='jet', method:int=cv2.TM_CCOEFF_NORMED) -> FuncAnimation:
+    
+    FrameType = tuple[int, int, np.ndarray, np.ndarray]
+    def update(frame:FrameType) -> None:
+        for ax in axes[1:]: ax.cla()
+        i, j, match, img  = frame
+        fig.suptitle(f'i={i}, j={j}')
+        axes[1].imshow(img)
+        axes[2].imshow(match, extent=[0, result_w, 0, result_h], vmin=min_value, vmax=max_value, cmap=cmap)
+        
+    def send_frames() -> Generator[tuple[int, int, np.ndarray], None, None]:
+        for i in range((height-fg_height)//col_step):
+            col_progress = i*col_step
+            for j in range((width-fg_width)//row_step): #横
+                row_progress = j*row_step
+                bg_img = img.copy()
+                bg_img[col_progress:col_progress+fg_height, row_progress:row_progress+fg_width] = extracted_img
+                result_anim[:col_progress+1, :row_progress+1] = result[:col_progress+1, :row_progress+1]
+                yield col_progress, row_progress, result_anim, bg_img
+
+    fig, axes = plt.subplots(3, 1)
+    cmap = get_cmap(cmap)
     height, width, _ = img.shape
     fg_height, fg_width, _ = extracted_img.shape
-    return_values:list[AxesImage] = []
-    for i in range((width-fg_width)//row_step): #横
-        row_progress = i*row_step
-        for j in range((height-fg_height)//col_step):
-            col_progress = j*col_step
-            bg_img = img.copy()
-            bg_img[col_progress:col_progress+fg_height, row_progress:row_progress+fg_width] = extracted_img
-            cv2.imshow('img', bg_img)
-            cv2.waitKey(1)
-    
-file_path = os.path.join(__file__, '..', 'dropkick.jpg')
-img = cv2.imread(file_path)
-extractor = ImageExtractor(img); extractor.mainloop()
-extracted_img = extractor.result
+    result = cv2.matchTemplate(img, extracted_img, method=method) #結果は先に計算
+    result_h, result_w = result.shape
+    min_value, max_value, _, max_idx = cv2.minMaxLoc(result)
+    result_anim = np.zeros((result.shape))
+    left_top_x, left_top_y = max_idx
+    axes[0].imshow(cv2.rectangle(img.copy(), (left_top_x, left_top_y), (left_top_x+fg_width, left_top_y+fg_height), (0, 0, 255), 3))
+    axes[0].set(xticks=[], yticks=[])
+    axes[2].set(xlim=[0, result_w], ylim=[0, result_h])
+    anim = FuncAnimation(fig, update, send_frames, interval=1)
+    fig.colorbar(ScalarMappable(cmap=cmap, norm=Normalize(vmin=min_value, vmax=max_value)), orientation='horizontal')
+    return anim
+            
+if __name__ == '__main__':
+    file_path = os.path.join(__file__, '..', 'dropkick.jpg')
+    img = cv2.imread(file_path)
+    extractor = ImageExtractor(img); extractor.mainloop()
+    extracted_img = extractor.result
+    anim = animateMatchTemplate(img, extracted_img, row_step=15, col_step=15)
+    plt.show()
 
-h, w, _ = extracted_img.shape
 
-result = cv2.matchTemplate(img, extracted_img, method=cv2.TM_CCORR_NORMED)
-plt.imshow(result)
-plt.show()
+
+
